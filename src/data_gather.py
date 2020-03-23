1efe5df187d7a556d6e364d25368d00719970805
Harish Anand <harishanand95@gmail.com>
2020-03-22 20:37:32 -0700

    Add: ARM and OFFBOARD service calls

--- src/data_gather.py
+++ src/data_gather.py
@@ -1,38 +1,43 @@
-from pynput.keyboard import Key, Listener
+# from pynput.keyboard import Key, Listener
 import rospy
+from geometry_msgs.msg import PoseStamped
+from mavros_msgs.msg import State, PositionTarget
+import math
+from mavros_msgs.srv import SetMode, CommandBool
+from mavros_test_common import MavrosTestCommon
 
-def on_press(key):
-    print('{0} pressed'.format(
-        key))
-
-def on_release(key):
-    print('{0} release'.format(
-        key))
-    if key == Key.esc:
-        # Stop listener
-        return False
-
-# Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+# def on_press(key):
+#     print('{0} pressed'.format(
+#         key))
+#
+# def on_release(key):
+#     print('{0} release'.format(
+#         key))
+#     if key == Key.esc:
+#         # Stop listener
+#         return False
+#
+# # Collect events until released
+# with Listener(
+#         on_press=on_press,
+#         on_release=on_release) as listener:
+#     listener.join()
 
 
 class OffboardControl:
     """ PX4 UAV controller works in offboard mode"""
     def __init__(self):
         self.curr_pose = PoseStamped()
-        self.isReadyToFly = False
+        self.is_ready_to_fly = False
         self.hover_loc = [0, 0, 7, 0, 0, 0, 0]
         self.mode = "HOVER"
+        self.dist_threshold = 0.4
 
         # define ros subscribers and publishers
         rospy.init_node('OffboardControl', anonymous=True)
         self.pose_sub = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, callback=self.pose_callback)
         self.state_sub = rospy.Subscriber('/mavros/state', State, callback=self.state_callback)
         self.vel_pub = rospy.Publisher('/mavros/setpoint_raw/local', PositionTarget, queue_size=10)
-
         # state machine code defined in controller function
         self.controller()
 
@@ -41,10 +46,37 @@ class OffboardControl:
 
     def state_callback(self, msg):
         if msg.mode == 'OFFBOARD':
-            self.isReadyToFly = True
+            self.is_ready_to_fly = True
         else:
-            print(msg.mode)
+            self.take_off()
 
+    def set_offboard_mode(self):
+        rospy.wait_for_service('/mavros/set_mode')
+        try:
+            flightModeService = rospy.ServiceProxy('/mavros/set_mode', SetMode)
+            isModeChanged = flightModeService(custom_mode='OFFBOARD')
+        except rospy.ServiceException as e:
+            print("service set_mode call failed: %s. OFFBOARD Mode could not be set. Check that GPS is enabled" % e)
+
+    def set_arm(self):
+        rospy.wait_for_service('/mavros/cmd/arming')
+        try:
+            armService = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+            armService(True)
+        except rospy.ServiceException as e:
+            print("Service arm call failed: %s" % e)
+
+    def set_disarm(self):
+        rospy.wait_for_service('/mavros/cmd/arming')
+        try:
+            armService = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+            armService(False)
+        except rospy.ServiceException as e:
+            print("Service disarm call failed: %s" % e)
+
+    def take_off(self):
+        self.set_offboard_mode()
+        self.set_arm()
 
     def hover(self):
         """ hover at height mentioned in location
@@ -61,11 +93,11 @@ class OffboardControl:
                location,
                location]
 
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(20)
         rate.sleep()
         shape = len(loc)
         pose_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
-        des_pose = self.copy_pose(self.curr_pose)
+        des_pose = PoseStamped()
         waypoint_index = 0
         sim_ctr = 1
 
@@ -73,8 +105,8 @@ class OffboardControl:
             if waypoint_index == shape:
                 waypoint_index = 0
                 sim_ctr += 1
-                print("HOVER STOP COUNTER: " + str(sim_ctr))
-            if self.isReadyToFly:
+                print("HOVER COUNTER: " + str(sim_ctr))
+            if self.is_ready_to_fly:
 
                 des_x = loc[waypoint_index][0]
                 des_y = loc[waypoint_index][1]
@@ -93,10 +125,11 @@ class OffboardControl:
 
                 dist = math.sqrt((curr_x - des_x)*(curr_x - des_x) + (curr_y - des_y)*(curr_y - des_y) +
                                  (curr_z - des_z)*(curr_z - des_z))
-                if dist < self.distThreshold:
+                if dist < self.dist_threshold:
                     waypoint_index += 1
 
             pose_pub.publish(des_pose)
+
             rate.sleep()
 
 
@@ -104,3 +137,7 @@ class OffboardControl:
         while not rospy.is_shutdown():
             if self.mode == "HOVER":
                 self.hover()
+                
+                
+if __name__ == "__main__":
+    OffboardControl()
